ef8fc731d598da3158c6e40d8501cae7dab912bb
Lucas Moccelin <moccelin.trab@gmail.com>
2022-03-24 11:20:54 -0300

    Finished magbank project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,23 +6,20 @@ import {
   faTwitter,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-
-import "./Footer.scss";
-
 import logo from "../assets/MAGbank.png";
 import applestore from "../assets/applestore.png";
 import googleplay from "../assets/googleplay.png";
+
+import "./Footer.scss";
 
 const Footer = () => (
   <footer className="footer">
     <Container>
       <Row className="text-center py-5">
-        <Col xs={12} lg={6} className="text-lg-start ">
-          {/* mb-5*/}
+        <Col xs={12} lg={6} className="text-lg-left mb-5 mb-lg-0">
           <Image src={logo} />
         </Col>
-        <Col xs={12} lg={4} className="py-xs-5">
-          {/* mb-5 */}
+        <Col xs={12} lg={4} className="mb-5 mb-lg-0">
           <Image src={applestore} />
           <Image src={googleplay} className="ml-2" />
         </Col>
@@ -31,18 +28,28 @@ const Footer = () => (
           lg={2}
           className="d-flex align-items-center justify-content-center"
         >
-          <FontAwesomeIcon icon={faFacebookSquare} color="a7c7c7c" size="2x" />
+          <a
+            href="http://facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              color="#7c7c7c"
+              size="2x"
+            />
+          </a>
           <FontAwesomeIcon
             icon={faTwitter}
-            color="a7c7c7c"
+            color="#7c7c7c"
             size="2x"
-            className="ms-2"
+            className="ml-2"
           />
           <FontAwesomeIcon
             icon={faYoutube}
-            color="a7c7c7c"
+            color="#7c7c7c"
             size="2x"
-            className="ms-2"
+            className="ml-2"
           />
         </Col>
       </Row>
